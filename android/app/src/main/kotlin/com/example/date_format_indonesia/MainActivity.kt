2f07f58a9cf026cb42a6f0ce35d28b5a67431a85
bahrulajisantoso <76810040+bahrulajisantoso@users.noreply.github.com>
2024-10-12 16:09:40 +0700

    inital commit

---
+++ android/app/src/main/kotlin/com/example/date_format_indonesia/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.date_format_indonesia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Date Format Indonesia',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Date Format Indonesia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    WidgetsFlutterBinding.ensureInitialized();
+    initializeDateFormatting('id_ID', '');
+    super.initState();
+  }
+
+  /// function date format
+  String formatDate() {
+    return DateFormat('EEEE, dd MMMM yyyy', 'id_ID').format(DateTime.now());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(formatDate()),
+      ),
+    );
+  }
+}
